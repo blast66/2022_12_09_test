86a7bd3a1f796d8b8298599d636fc08ac0b5e60d
blast66 <113650618+blast66@users.noreply.github.com>
2022-12-09 11:52:21 +0800

    Add files via upload

--- test33.java
+++ test33.java
@@ -1,14 +1,13 @@
 public class test33 {
     public static void main(String[] args){
         int count = 0;
+        int x = 0;
         int[] arr = {10,9,8,7,6,5,4,3,2,1,0};
-        for(int i = 0; i<= arr.length-1 ; i++){
-            count ++;
-            if(arr[i]==2){
-                System.out.println("index >> "+ i);
-                break;
-            }
+        while(true){
+            if ()
         }
+
+
         System.out.println("count >> "+ count);
     }
 }
